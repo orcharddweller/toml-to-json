79175649997ca1084210d1d9842ff4bc2ce2e978
marcinplatek <maruplat@gmail.com>
2023-11-14 10:00:33 +0100

    moved to smol-toml

--- src/lib/cm-theme.ts
+++ src/lib/cm-theme.ts
@@ -1,6 +1,6 @@
 import { EditorView } from '@codemirror/view';
 
 export const theme = EditorView.theme({
-	'&': { height: '100%' },
+	'&': { height: '100%', width: '100%' },
 	'.cm-scroller': { overflow: 'auto' }
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import cn from 'classnames';
-	import toml from '@ltd/j-toml';
+	import { parse } from 'smol-toml';
+
 	import TomlEditor from './TomlEditor.svelte';
 	import JsonEditor from './JsonEditor.svelte';
 
@@ -12,7 +13,7 @@
 	$: {
 		let parsed;
 		try {
-			parsed = toml.parse(tomlText, { bigint: false, joiner: '\n' });
+			parsed = parse(tomlText);
 			errorMessage = null;
 		} catch (e: unknown) {
 			if (!(e instanceof Error)) throw e;
@@ -30,7 +31,7 @@
 	<h1 class="text-5xl font-bold">Convert TOML to JSON</h1>
 
 	<div class="flex flex-row gap-2 w-full h-96">
-		<div class="flex-1">
+		<div class="flex-1 w-0">
 			<h2 class="text-3xl font-semibold">TOML</h2>
 			<TomlEditor class="border border-slate-400 h-full" bind:doc={tomlText} />
 			<div class="text-red-700">{errorMessage ?? ''}</div>
@@ -44,7 +45,7 @@
 			<div class="swap-on">➡</div>
 			<div class="swap-off">⬅</div>
 		</button>
-		<div class="flex-1">
+		<div class="flex-1 w-0">
 			<h2 class="text-3xl font-semibold">JSON</h2>
 			<JsonEditor class="border border-slate-400 h-full" bind:doc={jsonText} />
 		</div>
